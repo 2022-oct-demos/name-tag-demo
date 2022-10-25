8c062780ce24adab8a820e8fd6c9a2159eacfdfc
Beau Elliott <belliott1516@gmail.com>
2022-10-25 12:52:53 -0700

    added functionality to button and console.log button click

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ console.log('nameDisplay', nameDisplay);
 
 nameDisplay.textContent = 'Delaney';
 nameDisplay.classList.add('purple');
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i clicked this button');
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+});
